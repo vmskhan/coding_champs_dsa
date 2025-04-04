3c0fca81f828af1b52358d8908450964e9073393
sake123th <rathkantisaketh@gmail.com>
2025-04-04 22:51:38 +0530

    prob_5_app_1

---
+++ Problems_and_Solutions/day-4/cpp/prob_5_app_1.cpp
@@ -0,0 +1,10 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=1;i<=n;i++)
+    {
+        int a=1;
+        for(int j=n;j>=i;j--,a++)
+            cout<<a<<" ";
+        cout<<endl;
+    }
+}
