007151c707ed5a5add5e5b2ea60a86e68251162c
vmskhan <56332670+vmskhan@users.noreply.github.com>
2025-05-09 10:33:04 +0530

    fixed spaces in patterns-final

--- Problems_and_Solutions/day-8/Java/Solution6.java
+++ Problems_and_Solutions/day-8/Java/Solution6.java
@@ -2,29 +2,29 @@ class Solution6{
     public static void pattern(int n){
         for(int i=1; i<=n; i++){
             for(int j=i; j<=n; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             for(int j=1; j<i; j++){
-                System.out.print("  ");
+                System.out.print("    ");
             }
             for(int j=i; j<=n; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             for(int j=i; j<n; j++){
-                System.out.print("  ");
+                System.out.print("    ");
             }
             for(int j=1; j<=i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
-        }        
+        }     
     }
 
     public static void main(String args[]){
--- Problems_and_Solutions/days-10 to 19/day-10/Java/Solution6.java
+++ Problems_and_Solutions/days-10 to 19/day-10/Java/Solution6.java
@@ -9,7 +9,7 @@ class Solution6{
                 int bottom = size-i;
 
                 int min = Math.min(Math.min(top, bottom), Math.min(left, right));
-                System.out.print((n-min) + " ");
+                System.out.print((n-min));
             }
             System.out.println();
         }
